2efa8caf28c5a6b8f8ab2259f1311302f22cbb5d
otomon10 <otomono@gmail.com>
2019-05-28 20:31:00 +0900

    add publish cmd motor

--- motor_test/src/motor_test.cpp
+++ motor_test/src/motor_test.cpp
@@ -1,18 +1,32 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
+#include "std_msgs/Float32MultiArray.h"
+
+std_msgs::Float32MultiArray cmd_motor;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
-	ROS_INFO("I heard:");
+    cmd_motor.data[0] = msg->axes[1];
+    cmd_motor.data[1] = msg->axes[3];
 }
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "motor_test");
 	ros::NodeHandle n;
-
+    cmd_motor.data.resize(2);
+    
+    ros::Publisher pub = n.advertise<std_msgs::Float32MultiArray>("cmd_motor", 1000);
 	ros::Subscriber sub = n.subscribe("joy", 1000, joyCallback);
-	ros::spin();
+    ros::Rate loop_rate(10);
+
+    while(ros::ok())
+    {
+        pub.publish(cmd_motor);
+        ros::spinOnce();
+        loop_rate.sleep();
+
+    }
 
 	return 0;
 }
