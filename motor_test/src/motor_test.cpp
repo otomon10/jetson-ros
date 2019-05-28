fad9713dec08bff537becb8be5bea97201bc9e2e
otomon10 <otomono@gmail.com>
2019-05-29 00:12:45 +0900

    add: output motor GPIO.

--- motor_test/src/motor_test.cpp
+++ motor_test/src/motor_test.cpp
@@ -1,31 +1,26 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
-#include "std_msgs/Float32MultiArray.h"
+#include "motor.h"
 
-std_msgs::Float32MultiArray cmd_motor;
+Motor *motorL;
+Motor *motorR;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
-    cmd_motor.data[0] = msg->axes[1];
-    cmd_motor.data[1] = msg->axes[3];
+    motorL->run(msg->axes[1]);
+    motorR->run(msg->axes[3]);
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "motor_test");
-	ros::NodeHandle n;
-    cmd_motor.data.resize(2);
-    
-    ros::Publisher pub = n.advertise<std_msgs::Float32MultiArray>("cmd_motor", 1000);
-	ros::Subscriber sub = n.subscribe("joy", 1000, joyCallback);
-    ros::Rate loop_rate(10);
+    ros::init(argc, argv, "motor_test");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("joy", 1000, joyCallback);
 
-    while(ros::ok())
-    {
-        pub.publish(cmd_motor);
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+    motorL = new Motor(89, 202);
+    motorR = new Motor(187, 186);
+
+    ros::spin();
 
 	return 0;
 }
