696627e7348c6ecf3d25647c3af45c30ff0b5e70
otomon10 <otomono@gmail.com>
2019-05-29 22:03:34 +0900

    add: subsclibe /cmd_vel message.

--- motor_node/src/motor_node.cpp
+++ motor_node/src/motor_node.cpp
@@ -1,21 +1,34 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
+#include "geometry_msgs/Twist.h"
 #include "motor.h"
 
 std::unique_ptr<Motor> motorL;
 std::unique_ptr<Motor> motorR;
 
-void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
+void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-    motorL->run(msg->axes[1]);
-    motorR->run(msg->axes[3]);
+    motorL->run(joy->axes[1]);
+    motorR->run(joy->axes[3]);
+}
+
+void velCallback(const geometry_msgs::Twist::ConstPtr& vel)
+{
+    // only applies a given velocity for 1 second for safety reasons.
+    ros::Rate loop(1);
+    motorL->run(vel->linear.x); // TODO: calc m/s and rad.
+    motorR->run(vel->linear.x);
+    loop.sleep();
+    motorL->run(0);
+    motorR->run(0);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "motor_node");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("joy", 1000, joyCallback);
+    ros::Subscriber joySub = n.subscribe("joy", 10, joyCallback);
+    ros::Subscriber velSub = n.subscribe("cmd_vel", 10, velCallback);
 
     motorL.reset(new Motor(89, 202));
     motorR.reset(new Motor(187, 186));
